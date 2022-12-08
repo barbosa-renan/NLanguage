142ec97ea05de80d3e162841dd5842e9854b73b5
renan.q.barbosa <renan.q.barbosa@avanade.com>
2022-12-08 17:45:46 -0300

    feat: create translator

---
+++ src/NLanguage/Translator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Localization;
+
+namespace NLanguage
+{
+    public static class Translator<T>
+    {
+        private static readonly IStringLocalizer<T> _localizer;
+
+        public static string Translate(string term)
+        {
+            if (string.IsNullOrEmpty(term)) return term;
+
+            return _localizer[term].ToString();
+        }
+    }
+}
