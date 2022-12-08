9095039bf306e369290f20810ba23582aa7d84f3
renan.q.barbosa <renan.q.barbosa@avanade.com>
2022-12-08 17:32:31 -0300

    feat: create localizers

---
+++ src/NLanguage/TermLocalizer.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Localization;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace NLanguage
+{
+    internal class TermLocalizer : IStringLocalizer
+    {
+        private readonly IDistributedCache _cache;
+
+        private readonly JsonSerializer _serializer = new JsonSerializer();
+
+        public TermLocalizer(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public LocalizedString this[string name]
+        {
+            get
+            {
+                string value = GetTermByKey(name);
+                return new LocalizedString(name, value ?? name, value == null);
+            }
+        }
+
+        public LocalizedString this[string name, params object[] arguments]
+        {
+            get
+            {
+                var actualValue = this[name];
+                return !actualValue.ResourceNotFound
+                    ? new LocalizedString(name, string.Format(actualValue.Value, arguments), false)
+                    : actualValue;
+            }
+        }
+
+        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+        {
+            string filePath = $"Languages/{Thread.CurrentThread.CurrentCulture.Name}.json";
+            using (var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (var streamReader = new StreamReader(fileStream))
+            using (var jsonReader = new JsonTextReader(streamReader))
+            {
+                while (jsonReader.Read())
+                {
+                    if (jsonReader.TokenType != JsonToken.PropertyName) continue;
+
+                    string key = (string)jsonReader.Value;
+                    jsonReader.Read();
+                    string value = _serializer.Deserialize<string>(jsonReader);
+                    yield return new LocalizedString(key, value, false);
+                }
+            }
+        }
+
+        private string GetTermByKey(string key)
+        {
+            string relativeFilePath = $"Languages/{Thread.CurrentThread.CurrentCulture.Name}.json";
+            string fullFilePath = Path.GetFullPath(relativeFilePath);
+            if (File.Exists(fullFilePath))
+            {
+                string cacheKey = $"locale_{Thread.CurrentThread.CurrentCulture.Name}_{key}";
+                string cacheValue = _cache.GetString(cacheKey);
+
+                if (!string.IsNullOrEmpty(cacheValue)) 
+                    return cacheValue;
+
+                string result = GetValueFromJSON(key, Path.GetFullPath(relativeFilePath));
+
+                if (!string.IsNullOrEmpty(result)) 
+                    _cache.SetString(cacheKey, result);
+
+                return result;
+            }
+
+            return default(string);
+        }
+
+        private string GetValueFromJSON(string propertyName, string filePath)
+        {
+            if (propertyName == null) return default;
+            if (filePath == null) return default;
+            using (var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (var streamReader = new StreamReader(fileStream))
+            using (var jsonReader = new JsonTextReader(streamReader))
+            {
+                while (jsonReader.Read())
+                    if (jsonReader.TokenType == JsonToken.PropertyName && (string)jsonReader.Value == propertyName)
+                    {
+                        jsonReader.Read();
+                        return _serializer.Deserialize<string>(jsonReader);
+                    }
+
+                return default;
+            }
+        }
+    }
+}
---
+++ src/NLanguage/TermLocalizerFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Localization;
+using System;
+
+namespace NLanguage
+{
+    public class TermLocalizerFactory : IStringLocalizerFactory
+    {
+        private readonly IDistributedCache _cache;
+
+        public TermLocalizerFactory(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public IStringLocalizer Create(Type resourceSource) =>
+            new TermLocalizer(_cache);
+
+        public IStringLocalizer Create(string baseName, string location) =>
+            new TermLocalizer(_cache);
+    }
+}
