47935fc077416fcf20d2eb914fa3415d881e9c4f
renan.q.barbosa <renan.q.barbosa@avanade.com>
2022-12-15 13:33:19 -0300

    feat: add sample

---
+++ sample/src/SampleAPI/Controllers/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLanguage.Tools;
+
+namespace SampleAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        private readonly ITranslator _translator;
+
+        public HelloController(ITranslator translator)
+        {
+            _translator = translator;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return _translator.Translate("hello");
+        }
+    }
+}
